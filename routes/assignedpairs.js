2dee826ea9fce5e3c50cc93c14211a5b5f0586e1
Mike O'Connell <michael.p.oconnell@gmail.com>
2018-02-16 18:26:02 -0500

    Settled on how to do URLs, assignedpairs as example

--- routes/assignedpairs.js
+++ routes/assignedpairs.js
@@ -69,7 +69,7 @@ router.get("/", function(req, res) {
 });
 
 /* POST to Set AssignedPair Service */
-router.post('setassignedpair', function(req, res) {
+router.post('/setassignedpair', function(req, res) {
 	var thisFuncName = "setassignedpair: ";
 	
 	// Log message so we can see on the server side when we enter this

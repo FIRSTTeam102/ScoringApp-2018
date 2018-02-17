624fc01e4303719aea6370c4df7f35bbd7237df1
GEARHEADS-8\first <firstteam102@gmail.com>
2018-02-17 10:41:41 -0500

    Added login feature/bcrypt

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+
+router.get('/adduser', function(req, res){
+	
+	
+	
+	res.render('./login', { 
+		tournament: 'sdlfkjdslk',
+		title: "Create User"
+	});
+	
+});
+router.post('/adduser', function(req, res){
+	
+	var name = req.body.username;
+	var pass = req.body.password;
+	
+	console.log(name);
+	console.log(pass);
+	
+	const saltRounds = 10;
+	const myPlaintextPassword = pass;
+	const someOtherPlaintextPassword = 'not_bacon';
+	
+	bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+		// Store hash in your password DB.
+		
+		var db = req.db;
+		var collection = db.get("adminusers");
+		
+		collection.insert({
+			"username": name,
+			"password": hash
+		});
+	});
+	
+	res.redirect('/login/adduser');
+});
 
 router.get('/scouter', function(req, res) {
 	
@@ -38,6 +76,37 @@ router.get('/admin', function(req, res) {
 
 router.post('/admin', function(req, res) {
 	
+	var username = req.body.username;
+	var password = req.body.password;
+	
+	var db = req.db;
+	var collection = db.get("adminusers");
+	
+	collection.find({
+		"username": username
+	},{}, function(err, user){
+		if(err){console.log(err);}
+		
+		if(user[0] != undefined){
+			var hash = user[0].password;
+			console.log(hash);
+			
+			bcrypt.compare( password, hash, function(err, output){
+				if(err){console.log(err);}
+				
+				if(output == true){
+					res.send("LOGGED IN");
+				}else{
+					res.send("NOT LOGGED IN");
+				}
+			});
+			
+		}else{
+			res.send("USER DOES NOT EXIST");
+		}
+	});
+	
+	/*
 	if(req.body.username == "test" && req.body.password == "pass"){
 		req.cookies.isLoggedIn = "true";
 		console.log("logged in");
@@ -53,7 +122,7 @@ router.post('/admin', function(req, res) {
 			error: "true"
 		});
 	}
-	res.end();
+	*/
 });
 
 module.exports = router;

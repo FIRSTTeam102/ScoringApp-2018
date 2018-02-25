4d41f22446f86096438bd620d1db94ebb0bfc59e
Mike O'Connell <michael.p.oconnell@gmail.com>
2018-02-25 18:11:54 -0500

    Added REST calls & UI for events from TBA API

--- routes/externaldata.js
+++ routes/externaldata.js
@@ -5,6 +5,8 @@ router.get("/events", function(req, res) {
 	var thisFuncName = "externaldata.events[get]: ";
 	console.log(thisFuncName + 'ENTER')
 	
+	var events = {};
+	
     // Set our internal DB variable
     var db = req.db;
 
@@ -18,11 +20,30 @@ router.get("/events", function(req, res) {
 	console.log(thisFuncName + 'Year: ' + year);
 
 	// Read events from DB for current year
-	
-	// Read unique list of years in DB
+	var eventCol = db.get("events");
+	eventCol.find({"year": parseInt(year)},{sort: {"start_date": 1, "end_date": 1, "name": 1}}, function(e, docs){
+		
+		if(e){ //if error, log to console
+			console.log(e);
+		}
+		events = docs;
+		
+		// Read unique list of years in DB
+		var uniqueYears;
+		eventCol.distinct("year", function(e, docs) {
+			uniqueYears = docs.sort();
+			console.log(thisFuncName + "uniqueYears=" + uniqueYears);
+			
+			res.render("./events", {
+				"events": events,
+				"years": uniqueYears,
+				"selectedYear": year
+			});
+		});
+	});
 	
 	// Send to Pug page to display
-	res.redirect("/");
+	//res.render("./events?year=" + year);
 });
 
 router.post("/events", function(req, res) {
@@ -31,6 +52,7 @@ router.post("/events", function(req, res) {
 	
     // Set our internal DB variable
     var db = req.db;
+	var eventCol = db.get("events");
 
     // Get our form value(s)
     var year = req.body.year;
@@ -38,31 +60,63 @@ router.post("/events", function(req, res) {
 	console.log(thisFuncName + 'year=' + year)
 	
 	// nodeclient from earlier?
-	//var Client = require('node-rest-client').Client;
-	//var client = new Client();
+	var Client = require('node-rest-client').Client;
+	var client = new Client();
 	
+//		path: { "year" : year },
 	var args = {
-//		path: { "event" : eventCode },
 		headers: { "accept": "application/json", "X-TBA-Auth-Key": "iSpbq2JH2g27Jx2CI5yujDsoKYeC8pGuMw94YeK3gXFU6lili7S2ByYZYZOYI3ew" }
 	}
+	var url = "https://www.thebluealliance.com/api/v3/events/" + year + "/simple";
+	console.log(thisFuncName + "url=" + url);
 	
-	client.get("https://www.thebluealliance.com/api/v3/event/${year}/simple", args, function (data, response) {
-        // parsed response body as js object 
-		var array = JSON.parse(data);
-		var arrayLength = array.length;
-		console.log(thisFuncName + 'Found ' + arrayLength + ' data for year ' + year);
-		for (var i = 0; i < arrayLength; i++) {
-			//console.log(teamArray[i]);
-			console.log('#' + i + ': key=' + array[i].key + ', name=' + array[i].name);
-		}		
-		//console.log("********************** DATA **********************");
-        //console.log(teamArray);
-        // raw response 
-		//console.log("==================== RESPONSE ====================");
-        //console.log(response);
-    });
+	// Read unique list of years in DB
+	var uniqueYears;
+	eventCol.distinct("year", function(e, docs) {
+		uniqueYears = docs.sort();
 	
-	res.redirect("/");
+		client.get(url, args, function (data, response) {
+			var array = JSON.parse(data);
+			var arrayLength = array.length;
+			if (arrayLength == null)
+			{
+				console.log(thisFuncName + "Whoops, there was an error!")
+				console.log(thisFuncName + "data=" + data);
+				year = (new Date()).getFullYear();
+				
+				res.render("./events", {
+					"years": uniqueYears,
+					"selectedYear": year
+				});
+			}
+			else
+			{
+				console.log(thisFuncName + 'Found ' + arrayLength + ' data for year ' + year);
+				//console.log(thisFuncName + 'Stringify array: ' + JSON.stringify(array));
+				
+				// First delete existing event data for the given year
+				eventCol.remove({"year": parseInt(year)}, function(e, docs) {
+					// Now, insert the new data
+					eventCol.insert(array, function(e, docs) {
+						// Then read it back in order
+						eventCol.find({"year": parseInt(year)},{sort: {"start_date": 1, "end_date": 1, "name": 1}}, function(e, docs){
+							
+							// Re-read the unique years (in case we just added a new one)
+							eventCol.distinct("year", function(e, docs) {
+								uniqueYears = docs.sort();
+
+								res.render("./events", {
+									"events": docs,
+									"years": uniqueYears,
+									"selectedYear": year
+								});
+							});
+						});
+					});
+				});
+			}
+		});
+	});
 });
 
 module.exports = router;

982931f55aa4ee19ba1746a0e7d9ed907b26e57a
Mike O'Connell <michael.p.oconnell@gmail.com>
2018-02-26 17:28:23 -0500

    Adding loading teams into DB based on event data

--- routes/externaldata.js
+++ routes/externaldata.js
@@ -274,20 +274,14 @@ router.post("/teams", function(req, res) {
 	
     // Set our internal DB variable
     var db = req.db;
-	var matchCol = db.get("matches");
 	var eventCol = db.get("events");
+	var teamCol = db.get("teams");
 
     // Get our form value(s)
     var eventId = req.body.eventId;
 
 	console.log(thisFuncName + 'eventId=' + eventId);
 
-	//// TODO
-	// Read teams from TBA
-	// Read teams from DB
-	// If any TBA teams are not yet in DB, add them to DB
-	// Return all teams to client
-	
 	// nodeclient from earlier?
 	var Client = require('node-rest-client').Client;
 	var client = new Client();
@@ -296,7 +290,8 @@ router.post("/teams", function(req, res) {
 		headers: { "accept": "application/json", "X-TBA-Auth-Key": "iSpbq2JH2g27Jx2CI5yujDsoKYeC8pGuMw94YeK3gXFU6lili7S2ByYZYZOYI3ew" }
 	}
 
-	var url = "https://www.thebluealliance.com/api/v3/event/" + eventId + "/matches";
+	// Read teams from TBA
+	var url = "https://www.thebluealliance.com/api/v3/event/" + eventId + "/teams/simple";
 	console.log(thisFuncName + "url=" + url);
 	
 	// Read unique list of years in DB (for redirect)
@@ -320,19 +315,54 @@ router.post("/teams", function(req, res) {
 			}
 			else
 			{
-				console.log(thisFuncName + 'Found ' + arrayLength + ' data for event ' + eventId);
-				//console.log(thisFuncName + 'Stringify array: ' + JSON.stringify(array));
+				var tbaTeams = array;
+				//console.log(thisFuncName + "tbaTeamArray=" + JSON.stringify(tbaTeamArray));
+				console.log(thisFuncName + 'Found ' + tbaTeams.length + ' teams from TBA for event ' + eventId);
+				//for (let team of tbaTeams) { console.log(thisFuncName + 'tbaTeam.key=' + team.key); }
 				
-				// First delete existing match data for the given event
-				matchCol.remove({"event_key": eventId}, function(e, docs) {
-					// Now, insert the new data
-					matchCol.insert(array, function(e, docs) {
-						// Then read it back in order
-						matchCol.find({"event_key": eventId},{sort: {"time": 1}}, function(e, docs){
-							var matches = docs;
-							
-							res.render("./matches", {
-								"matches": matches
+				// Read teams from DB
+				teamCol.find({},{sort: {"key": 1}}, function(e, docs){
+					
+					if(e){ //if error, log to console
+						console.log(e);
+					}
+					var dbTeams = docs;
+					//console.log(thisFuncName + "dbTeams=" + JSON.stringify(dbTeams));
+					console.log(thisFuncName + 'Found ' + dbTeams.length + ' teams in DB');
+					//for (let team of dbTeams) { console.log(thisFuncName + 'dbTeam.key=' + team.key); }
+
+					// collect which teams are already in DB
+					var dbTeamLookup = {};
+					for (var i = 0, len = dbTeams.length; i < len; i++) {
+						dbTeamLookup[dbTeams[i].key] = dbTeams[i];
+					}
+					
+					// If any TBA teams are not yet in DB, add them to a new collection for mass insert
+					var tbaTeamsInsert = [];
+					for (let team of tbaTeams)
+					{
+						//console.log(thisFuncName + 'tbaTeam.key=' + team.key + '; in dbTeamLookup?=' + dbTeamLookup[team.key]);
+						if (!dbTeamLookup[team.key])
+						{
+							//console.log(thisFuncName + '...adding ' + team.key);
+							tbaTeamsInsert.push(team);
+						}
+					}
+					console.log(thisFuncName + 'Found ' + tbaTeamsInsert.length + ' teams to add to DB');
+					/*
+					for (let team of tbaTeamsInsert)
+					{
+						console.log(thisFuncName + 'tbaTeamsInsert contains ' + team.key);
+					}
+					*/
+
+					// insert the new teams
+					teamCol.insert(tbaTeamsInsert, function(e, docs) {
+						// Re-read and return all teams to client
+						teamCol.find({},{sort: {"key": 1}}, function(e, docs){
+						
+							res.render("./teams", {
+								"teams": docs
 							});
 						});
 					});

c7a390e813ee2b1afadf992891aa559fc5f1c611
Mike O'Connell <michael.p.oconnell@gmail.com>
2018-02-26 18:58:47 -0500

    Added a set current eventId to admin index

--- routes/adminindex.js
+++ routes/adminindex.js
@@ -17,4 +17,25 @@ router.get('/', function(req, res) {
 
 });
 
+router.post('/setcurrent', function(req, res) {
+	var thisFuncName = "adminindex.setcurrent[post]: ";
+	var eventId = req.body.eventId;
+	console.log(thisFuncName + 'ENTER eventId=' + eventId);
+	
+    // Set our internal DB variable
+    var db = req.db;
+
+	var currentCol = db.get("current");
+	
+	// Remove the previous 'current' data
+	currentCol.remove({}, function(e, docs) {
+		// Now, insert the new data
+		currentCol.insert({"event": eventId}, function(e, docs) {
+			res.render('./adminindex', { 
+				title: 'Admin pages' 
+			});
+		});
+	});
+});
+
 module.exports = router;

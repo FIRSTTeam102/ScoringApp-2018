0a046c2ecfb87c6919f69ff223f6574423163e65
GEARHEADS-8\first <firstteam102@gmail.com>
2018-02-26 20:15:35 -0500

    finished ajax thing ye

--- public/js/ajax-sample.js
+++ public/js/ajax-sample.js
@@ -3,41 +3,12 @@ $(function(){
 	$("#submit").on('click', function(){
 		
 		var data_text = $("#text").val();
-		localStorage.setItem("ajaxsample-text", 
+		localStorage.setItem("ajaxsample-text", data_text); 
 		
-		submitData("/ajax-exampple/submit", {
-			text: localStorage.getItem("ajaxsample-text"
+		submitData("/ajax-example/submit", {
+			text: "ajaxsample-text"
 		});
 		
 	});
 
 });
-
-function submitData(url, content){
-	
-	createNotificationCard( "Submitting data..." );
-	
-	
-	$.post("/ajax-example/submit", {
-		
-		text: data_text
-		
-	}, function(res, status){
-		
-		if(status == "success"){
-			
-			createNotificationCard("Data successfully sent. Respone: " + res.data, "good");
-		}
-		else if(status == "timeout"){
-			
-			createNotificationCard("Request timed out. Retrying...", "warn");
-			//retries
-			submitData();
-		}
-		else{
-			
-			createNotificationCard("Error", "alert");
-			console.error(data, status);
-		}
-	});
-}

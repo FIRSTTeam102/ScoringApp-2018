6a913ce7e8d7fef11353d956f6c1501a7e42837f
Mike O'Connell <michael.p.oconnell@gmail.com>
2018-02-27 19:54:49 -0500

    Date formatted on current matches; further rendering fixes; added button to update all matches & moved RESET button to bottom

--- routes/current.js
+++ routes/current.js
@@ -143,4 +143,75 @@ router.post("/updatematch", function(req, res) {
 	});
 });
 
+router.post("/updatematches", function(req, res) {
+	var thisFuncName = "current.updatematches[post]: ";
+	console.log(thisFuncName + 'ENTER')
+	
+    // Set our internal DB variable
+    var db = req.db;
+	var matchCol = db.get("matches");
+	var currentCol = db.get("current");
+	
+	var matchId = req.body.matchId;
+
+	// get the 'current' event from DB
+	currentCol.find({}, {}, function(e, docs) {
+		var noEventFound = 'No event defined';
+		var eventId = noEventFound;
+		if (docs)
+			if (docs.length > 0)
+				eventId = docs[0].event;
+		if (eventId === noEventFound)
+		{
+			res.render('./adminindex', { 
+				title: 'Admin pages',
+				current: eventId
+			});
+		}
+		
+		// nodeclient
+		var Client = require('node-rest-client').Client;
+		var client = new Client();
+		
+		var args = {
+			headers: { "accept": "application/json", "X-TBA-Auth-Key": "iSpbq2JH2g27Jx2CI5yujDsoKYeC8pGuMw94YeK3gXFU6lili7S2ByYZYZOYI3ew" }
+		}
+		var url = "https://www.thebluealliance.com/api/v3/event/" + eventId + "/matches";
+		console.log(thisFuncName + "url=" + url);
+		client.get(url, args, function (data, response) {
+			var array = JSON.parse(data);
+			var arrayLength = array.length;
+			if (arrayLength == null)
+			{
+				console.log(thisFuncName + "Whoops, there was an error!")
+				console.log(thisFuncName + "data=" + data);
+				
+				res.render('./adminindex', { 
+					title: 'Admin pages',
+					current: eventId
+				});
+			}
+			else
+			{
+				console.log(thisFuncName + 'Found ' + arrayLength + ' data for event ' + eventId);
+				
+				// First delete existing match data for the given event
+				matchCol.remove({"event_key": eventId}, function(e, docs) {
+					// Now, insert the new data
+					matchCol.insert(array, function(e, docs) {
+						// Then read it back in order
+						matchCol.find({"event_key": eventId},{sort: {"time": 1}}, function(e, docs){
+							var matches = docs;
+							
+							res.render("./currentmatches", {
+								"matches": matches
+							});
+						});
+					});
+				});
+			}
+		});
+	});
+});
+
 module.exports = router;

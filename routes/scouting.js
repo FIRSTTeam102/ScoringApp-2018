03ee09df58b57d5c2d5cf0e283406f1704df59ca
Mike O'Connell <michael.p.oconnell@gmail.com>
2018-03-09 21:28:00 -0500

    Initial cut for live pit and match scouting - modular dynamic pages, write data to DB, etc.

--- routes/scouting.js
+++ routes/scouting.js
@@ -1,6 +1,130 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/match*', function(req, res) {
+	var thisFuncName = "scouting.match*[get]: ";
+	console.log(thisFuncName + 'ENTER');
+	
+	var thisUser = req.user;
+	var thisUserName = thisUser.name;
+
+	var matchKey = req.query.key;
+	if (!matchKey) {
+		res.redirect("/dashboard");
+		return;
+	}
+	console.log(thisFuncName + 'matchKey=' + matchKey + ' ~ thisUserName=' + thisUserName);
+	
+	var db = req.db;
+	var collection = db.get("scoringlayout");
+	collection.find({}, {sort: {"order": 1}}, function(e, docs){
+		var layout = docs;
+		console.log(layout);
+		res.render("./scouting/match", {
+			layout: layout,
+			key: matchKey
+		});
+	});
+});
+
+router.post('/submitmatch', function(req, res) {
+	var thisFuncName = "scouting.submitmatch[post]: ";
+	console.log(thisFuncName + 'ENTER');
+	
+	var thisUser = req.user;
+	var thisUserName = thisUser.name;
+	
+	//console.log(thisFuncName + 'req.body=' + JSON.stringify(req.body));
+	
+	var matchData = req.body;
+	
+	var matchKey = matchData.matchkey;
+	delete matchData.matchkey;
+	console.log(thisFuncName + 'matchKey=' + matchKey + ' ~ thisUserName=' + thisUserName);
+	console.log(thisFuncName + 'matchData=' + JSON.stringify(matchData));
+
+	var db = req.db;
+    var matchCol = db.get('scoringdata');
+
+	matchCol.update( { "match_team_key" : matchKey }, { $set: { "data" : matchData, "actual_scorer": thisUserName } }, function(e, docs){
+		res.redirect("/dashboard");
+	});
+});
+
+router.get('/pit*', function(req, res) {
+	var thisFuncName = "scouting.pit*[get]: ";
+	console.log(thisFuncName + 'ENTER');
+	
+	var thisUser = req.user;
+	var thisUserName = thisUser.name;
+
+	var teamKey = req.query.team;
+	if (!teamKey) {
+		res.redirect("/dashboard");
+		return;
+	}
+	console.log(thisFuncName + 'teamKey=' + teamKey + ' ~ thisUserName=' + thisUserName);
+	
+	var db = req.db;
+	var collection = db.get("scoutinglayout");
+	
+	collection.find({}, {sort: {"order": 1}}, function(e, docs){
+		var layout = docs;
+		console.log(layout);
+		res.render("./scouting/pit", {
+			layout: layout,
+			key: teamKey
+		});
+	});
+});
+
+router.post('/submitpit', function(req, res) {
+	var thisFuncName = "scouting.submitpit[post]: ";
+	console.log(thisFuncName + 'ENTER');
+	
+	var thisUser = req.user;
+	var thisUserName = thisUser.name;
+	
+	//console.log(thisFuncName + 'req.body=' + JSON.stringify(req.body));
+	
+	var pitData = req.body;
+	
+	var teamKey = pitData.teamkey;
+	delete pitData.teamkey;
+	console.log(thisFuncName + 'teamKey=' + teamKey + ' ~ thisUserName=' + thisUserName);
+	console.log(thisFuncName + 'pitData=' + JSON.stringify(pitData));
+
+	var db = req.db;
+    var pitCol = db.get('scoutingdata');
+	var currentCol = db.get("current");
+
+	//
+	// Get the 'current' event from DB
+	//
+	currentCol.find({}, {}, function(e, docs) {
+		var noEventFound = 'No event defined';
+		var eventId = noEventFound;
+		if (docs)
+			if (docs.length > 0)
+				eventId = docs[0].event;
+		if (eventId === noEventFound) {
+			res.render('/adminindex', { 
+				title: 'Admin pages',
+				current: eventId
+			});
+		}
+		var event_key = eventId;
+
+		//res.redirect("/dashboard");
+		
+		pitCol.update( { "event_key" : event_key, "team_key" : teamKey }, { $set: { "data" : pitData, "actual_scouter": thisUserName } }, function(e, docs){
+			res.redirect("/dashboard");
+		});
+	});
+});
+
+///////// Previous code
+
 router.get('/', function(req, res){
 	
 	var teams = req.db.get('teams');

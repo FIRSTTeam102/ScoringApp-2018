192497a1601c828167c01cf16dcdec477bd8c0f6
GEARHEADS-8\first <firstteam102@gmail.com>
2018-03-12 19:13:38 -0400

    no message

--- routes/reports.js
+++ routes/reports.js
@@ -2,7 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/upcoming", function(req, res){
-	console.log("hi");
+	
+	if(!req.query || req.query.team){
+		
+	}
+	
 });
 
-module.exports - router;
+module.exports = router;

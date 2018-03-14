15041d0d1dd47625d9ff04244a43ddceddda1f2c
Jordan Lees <firstteam102@gmail.com>
2018-03-14 01:46:41 -0400

    I think I have a working data retension/submission program thingy.

--- public/js/match-client.js
+++ public/js/match-client.js
@@ -5,53 +5,32 @@ $(function(){
 		var formData = getFormData($("#matchform"));
 		var formDataString = JSON.stringify(formData);
 		
+		//adds data to local storage
 		localStorage.setItem("matchFormData", formDataString); 
 		
-		submitData("./match/submit", "matchFormData");
+		//data on item to submit
+		var toSubmit = {
+			url: "/scouting/match/submit",
+			dataKey: "matchFormData",
+			callback: function(){
+				console.log("Callback called from match-client.js");
+			}
+		};
+		//gets existing thingy from local storage
+		var toSubmitAlready = getToSubmit();
+		
+		if(toSubmitAlready){
+			console.log("toSubmitAlready");
+			toSubmitAlready.push(toSubmit);
+		}else{
+			console.log("not tsa");
+			toSubmitAlready = [toSubmit];
+		}
+		//sends to local storage
+		setToSubmit(toSubmitAlready);
+		
+		submitData(toSubmit.url, toSubmit.dataKey, toSubmit.callback);
 		
 	});
 
 });
-
-function getFormData($form){
-    var unindexed_array = $form.serializeArray();
-    var indexed_array = {};
-
-    $.map(unindexed_array, function(n, i){
-        indexed_array[n['name']] = n['value'];
-    });
-
-    return indexed_array;
-}
-
-function submitData(url, dataKey){
-	
-	var dataString = localStorage.getItem(dataKey);
-	if(!dataString)
-		return console.error("Failed to get data from localStorage");
-	console.log(dataString);
-	var data = JSON.parse(dataString);
-	console.log(data);
-	
-	$.post( url, data, function( res, status ){
-		var cardType;
-		
-		switch( res.status ){
-			case 200:
-				cardType = "good"; break;
-			case 500:
-				cardType = "bad"; break;
-		}
-		console.log(res);
-		
-		createNotificationCard( res.message, cardType );
-		
-	}).fail(function(){
-		console.warn("failed");
-		createNotificationCard("Failed to send data. Attempting to re-submit...", "warn");
-		setTimeout(function(){
-			submitData(url, dataKey);
-		}, 750);
-	});
-	
-}

78ffaf288c00c790a16e0fe2d286443e6b1f313d
Jordan Lees <firstteam102@gmail.com>
2018-03-24 00:05:55 -0400

    added cheeky team dropdown and removed bloody team numeric input :p

--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,38 @@ router.get('/', function(req, res) {
 	if(req.query)
 		var alert = req.query.alert || null;
 	
-	res.render('./index', { 
-		title: 'Home',
-		alert: alert
+	req.db.get('currentrankings').find({},{},function(e, rankings){
+		var teamList = [];
+		
+		if(e || !rankings || !rankings[0]){
+			console.error(e || "No current rankings");
+			return res.render('./index', { 
+				title: 'Home',
+				alert: alert
+			});
+		}
+		
+		for(var i = 0; i < rankings.length; i++){
+			teamList[i] = parseInt(rankings[i].team_key.substring(3));
+		}
+		teamList.sort(function(a,b){
+			if(a < b)
+				return -1;
+			if(a > b)
+				return 1;
+			// a must be equal to b
+			return 0;
+		})
+		
+		res.render('./index', { 
+				title: 'Home',
+				teamList: teamList,
+				alert: alert
+		});
 	});
+	
+
+	//console.log("Completed page in "+(Date.now()-req.requestTime)+" ms");
   
 });
 

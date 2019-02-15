4331be527d0e484fe710983163d94fe870cdcc9c
Jordan Lees <firstteam102@gmail.com>
2019-02-15 18:55:11 -0500

    fixed admin/data/teams

--- routes/admin/externaldata.js
+++ routes/admin/externaldata.js
@@ -58,35 +58,44 @@ router.get("/events", function(req, res) {
 /**
  * POST: Admin page to update all events for a given year.
  * @url POST: /admin/data/events
- * @view /events
+ * @redirect /admin/data/events
  */
 router.post("/events", function(req, res) {
 	if(!require('../checkauthentication')(req, res, 'admin'))
 		return null;
 	
 	var thisFuncName = "externaldata.events[post]: ";
-	res.log(thisFuncName + 'ENTER')
 	
     // Set our internal DB variable
     var db = req.db;
 	var eventCol = db.get("events");
-
+	
     // Get our form value(s)
     var year = req.body.year;
-
-	res.log(thisFuncName + 'year=' + year);
 	
-	// nodeclient from earlier?
-	var Client = require('node-rest-client').Client;
-	var client = new Client();
-	
-//		path: { "year" : year },
-	var args = {
-		headers: { "accept": "application/json", "X-TBA-Auth-Key": "iSpbq2JH2g27Jx2CI5yujDsoKYeC8pGuMw94YeK3gXFU6lili7S2ByYZYZOYI3ew" }
-	}
-	var url = "https://www.thebluealliance.com/api/v3/events/" + year + "/simple";
+	//Set up TBA url
+	var url = `https://www.thebluealliance.com/api/v3/events/${year}/simple`;
 	res.log(thisFuncName + "url=" + url);
 	
+	//Submit request to TBA
+	req.client.get(url, req.tbaRequestArgs, function(data, response){
+		
+		var events = JSON.parse(data);
+		//if request was invalid, redirect to admin page with alert message
+		if(events.length == undefined || events.length == 0){
+			return res.redirect("/admin?alert=Could not get events from TBA for specified year " + year);
+		}
+		
+		//Remove existing events list for year
+		eventCol.remove({ "year": parseInt(year) }, function(){
+			//Now insert new events list for year
+			eventCol.insert(events, function(){
+				//redirect back to events page
+				res.redirect(`/admin/data/events?year=${year}`);
+			});
+		});
+	});
+	/* OLD VERSION RRRRREEEEEEEEEE
 	// Read unique list of years in DB
 	var uniqueYears;
 	eventCol.distinct("year", function(e, docs) {
@@ -137,13 +146,13 @@ router.post("/events", function(req, res) {
 			}
 		});
 	});
+	*/
 });
-
 
 /**
  * Admin page to display matches of a specified event id.
- * @url /
- * @view /index
+ * @url /admin/data/matches
+ * @view /matches
  */
 router.get("/matches", function(req, res) {
 	if(!require('../checkauthentication')(req, res, 'admin'))
@@ -154,20 +163,20 @@ router.get("/matches", function(req, res) {
 	
 	var matches = {};
 	
-    // Set our internal DB variable
+    //Set up db and collections
     var db = req.db;
+	var matchCol = db.get("matches");
 
     // Get our query value(s)
     var eventId = req.query.eventId;
 	if (!eventId)
 	{
 		res.log(thisFuncName + 'No event specified');
-		res.redirect("./events");
+		res.redirect("/admin/data/events");
 	}
 	res.log(thisFuncName + 'eventId=' + eventId);
 
 	// Read matches from DB for specified event
-	var matchCol = db.get("matches");
 	matchCol.find({"event_key": eventId},{sort: {"time": 1}}, function(e, docs){
 		
 		if(e){ //if error, log to console
@@ -182,6 +191,12 @@ router.get("/matches", function(req, res) {
 	});
 });
 
+/**
+ * POST: Admin page to update match information for a given event.
+ * @url POST: /admin/data/matches
+ * @redirect /admin (to handle error)
+ * @redirect /admin/data/matches
+ */
 router.post("/matches", function(req, res) {
 	if(!require('../checkauthentication')(req, res, 'admin'))
 		return null;
@@ -196,69 +211,42 @@ router.post("/matches", function(req, res) {
 
     // Get our form value(s)
     var eventId = req.body.eventId;
-
 	res.log(thisFuncName + 'eventId=' + eventId);
 	
-	// nodeclient from earlier?
-	var Client = require('node-rest-client').Client;
-	var client = new Client();
-	
-	var args = {
-		headers: { "accept": "application/json", "X-TBA-Auth-Key": "iSpbq2JH2g27Jx2CI5yujDsoKYeC8pGuMw94YeK3gXFU6lili7S2ByYZYZOYI3ew" }
-	}
-
-	var url = "https://www.thebluealliance.com/api/v3/event/" + eventId + "/matches";
+	//Set up TBA api request
+	var url = `https://www.thebluealliance.com/api/v3/event/${eventId}/matches`;
 	res.log(thisFuncName + "url=" + url);
 	
-	// Read unique list of years in DB (for redirect)
-	var uniqueYears;
-	eventCol.distinct("year", function(e, docs) {
-		uniqueYears = docs.sort();
-	
-		client.get(url, args, function (data, response) {
-			var array = JSON.parse(data);
-			var arrayLength = array.length;
-			if (arrayLength == null)
-			{
-				res.log(thisFuncName + "Whoops, there was an error!")
-				res.log(thisFuncName + "data=" + data);
-				year = (new Date()).getFullYear();
+	//Request from TBA
+	req.client.get(url, req.tbaRequestArgs, function (data, response) {
+		
+		var matches = JSON.parse(data);
+		res.log(matches);
+		//if request was invalid, redirect to admin page with alert message
+		if(matches.length == undefined || matches.length == 0){
+			return res.redirect("/admin?alert=Could not get matches from TBA for specified event " + eventId);
+		}
+		
+		res.log(thisFuncName + 'Found ' + matches.length + ' data for event ' + eventId);
+		
+		// First delete existing match data for the given event
+		matchCol.remove({"event_key": eventId}, function(e, docs) {
+			// Now, insert the new data
+			matchCol.insert(matches, function(e, docs) {
 				
-				res.render("./events", {
-					title: "Events",
-					"years": uniqueYears,
-					"selectedYear": year
-				});
-			}
-			else
-			{
-				res.log(thisFuncName + 'Found ' + arrayLength + ' data for event ' + eventId);
-				//res.log(thisFuncName + 'Stringify array: ' + JSON.stringify(array));
-				
-				// First delete existing match data for the given event
-				matchCol.remove({"event_key": eventId}, function(e, docs) {
-					// Now, insert the new data
-					matchCol.insert(array, function(e, docs) {
-						// Then read it back in order
-						matchCol.find({"event_key": eventId},{sort: {"time": 1}}, function(e, docs){
-							var matches = docs;
-							
-							res.render("./matches", {
-								title: "Events",
-								"matches": matches
-							});
-						});
-					});
-				});
-			}
+				//redirect to matches page
+				res.redirect(`/admin/data/matches?eventId=${eventId}`);
+			});
 		});
 	});
 });
 
-////////////////////////
-// Teams by event
-////////////////////////
 
+/**
+ * Admin page to display all teams in local database.
+ * @url /admin/data/teams
+ * @view /teams
+ */
 router.get("/teams", function(req, res) {
 	if(!require('../checkauthentication')(req, res, 'admin'))
 		return null;
@@ -266,38 +254,66 @@ router.get("/teams", function(req, res) {
 	var thisFuncName = "externaldata.teams[get]: ";
 	res.log(thisFuncName + 'ENTER')
 	
-	var matches = {};
-	
     // Set our internal DB variable
     var db = req.db;
-
-	/*
-    // Get our query value(s)
-    var eventId = req.query.eventId;
-	if (!eventId)
-	{
-		res.log(thisFuncName + 'No event specified');
-		res.redirect("./events");
-	}
-	res.log(thisFuncName + 'eventId=' + eventId);
-	*/
-	
-	// Read all teams from DB
 	var teamCol = db.get("teams");
-	teamCol.find({},{sort: {"key": 1}}, function(e, docs){
+	
+	//return res.send(req.query.eventId);
+	
+	//if no event is specified send page with all teams
+	if(req.query.eventId == "" || req.query.eventId == undefined){
 		
-		if(e){ //if error, log to console
-			res.log(e);
-		}
-		teams = docs;
-		
-		res.render("./teams", {
-			title: "Teams",
-			"teams": teams
+		// Read all teams from DB
+		teamCol.find({},{sort: {"key": 1}}, function(e, teams){
+			//render page w/ all teams			
+			res.render("./teams", {
+				title: "All Teams",
+				"teams": teams,
+				header: "All Teams in Database"
+			});
 		});
-	});
+	}
+	//if event is specified, get list of teams from event
+	else{
+		//get eventId
+		var eventId = req.query.eventId;
+		
+		//prepare api call
+		var url = `https://www.thebluealliance.com/api/v3/event/${eventId}/teams/simple`;
+		
+		//perform api call
+		req.client.get(url, req.tbaRequestArgs, function(data, response){
+			
+			var teams = JSON.parse(data);
+			
+			//sort list of teams by number
+			teams.sort(function(a, b) {
+				let aNum = a.team_number;
+				let bNum = b.team_number;
+				if( aNum < bNum ){
+					return -1;
+				}
+				if( aNum > bNum ){
+					return 1;
+				}
+				return 0;
+			});
+			
+			//render page with sorted list of teams
+			res.render("./teams", {
+				title: `Teams in ${req.event.name}`,
+				"teams": teams
+			});
+		});
+	}
 });
 
+/**
+ * POST: Admin page to grab teams for a given event. (LEGACY)
+ * @url POST: /admin/data/teams
+ * @view /events
+ * @view /teams
+ */
 router.post("/teams", function(req, res) {
 	if(!require('../checkauthentication')(req, res, 'admin'))
 		return null;

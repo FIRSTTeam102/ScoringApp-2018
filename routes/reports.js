90d73bbfdf73aa62d5d9e97af51d4a96d1e7d1a9
Jordan Lees <firstteam102@gmail.com>
2019-02-16 14:18:44 -0500

    don't have to search for current event anymore because it is done inside useFunctions

--- routes/reports.js
+++ routes/reports.js
@@ -161,168 +161,151 @@ router.get("/teamintel", function(req, res){
 	var pitCol = req.db.get('scoutingdata');
 	var aggCol = req.db.get('scoringdata');
 	var matchCol = req.db.get('matches');
-	var currentCol = db.get("current");
 	var scoutCol = db.get("scoutinglayout");
 	var scoreCol = db.get("scoringlayout");
 	
-	//
-	// Get the 'current' event from DB
-	//
-	currentCol.find({}, {}, function(e, docs) {
-		var noEventFound = 'No event defined';
-		var eventId = noEventFound;
-		if (docs)
-			if (docs.length > 0)
-				eventId = docs[0].event;
-		if (eventId === noEventFound) {
-			res.render('/adminindex', { 
-				title: 'Admin pages',
-				current: eventId
+	var event_key = req.event.key;
+	var event_year = req.event.year;
+	res.log(event_year);
+	
+	// Team details
+	teamsCol.find({ "key" : teamKey }, {}, function(e, docs){
+		if(e)
+			return console.error(e);
+		if(!docs[0]){
+			return res.render('./error', {
+				title: "Intel: Team " + teamKey.substring(3),
+				error: {
+					status: "FRC Team "+teamKey.substring(3)+" does not exist or did not participate in this event."
+				}
 			});
 		}
-		// for later querying by event_key
-		var event_key = eventId;
-		res.log(thisFuncName + 'event_key=' + event_key);
-		
-		// Team details
-		teamsCol.find({ "key" : teamKey }, {}, function(e, docs){
-			if(e)
-				return console.error(e);
-			if(!docs[0]){
-				return res.render('./error', {
-					title: "Intel: Team " + teamKey.substring(3),
-					error: {
-						status: "FRC Team "+teamKey.substring(3)+" does not exist or did not participate in this event."
-					}
-				});
-			}
-			var team = docs[0];
+		var team = docs[0];
 
-			// Extract the current team ranking, etc.
-			rankCol.find({team_key: teamKey}, {sort: {rank: 1}}, function(e, docs) {
-				var ranking = null;
-				if (docs && docs.length > 0)
-					ranking = docs[0];
+		// Extract the current team ranking, etc.
+		rankCol.find({team_key: teamKey}, {sort: {rank: 1}}, function(e, docs) {
+			var ranking = null;
+			if (docs && docs.length > 0)
+				ranking = docs[0];
+		
+			// Pit scouting info
+			pitCol.find({ "event_key" : event_key, "team_key" : teamKey }, {}, function(e, docs){
+				var pitData = null;
+				var pitData1 = null;
+				if (docs && docs[0]) {
+					if (docs[0].data)
+						pitData = docs[0].data;
+					if (docs[0].data1)
+						pitData1 = docs[0].data1;
+				}
+				//res.log(thisFuncName + 'pitData=' + JSON.stringify(pitData));
 			
-				// Pit scouting info
-				pitCol.find({ "event_key" : event_key, "team_key" : teamKey }, {}, function(e, docs){
-					var pitData = null;
-					var pitData1 = null;
-					if (docs && docs[0]) {
-						if (docs[0].data)
-							pitData = docs[0].data;
-						if (docs[0].data1)
-							pitData1 = docs[0].data1;
-					}
-					//res.log(thisFuncName + 'pitData=' + JSON.stringify(pitData));
-				
-					// Pit data layout
-					scoutCol.find({}, {sort: {"order": 1}}, function(e, docs){
-						var layout = docs;
-						//res.log(thisFuncName + 'layout=' + JSON.stringify(layout));
-						
-						// Pull in individual scouting data for this team, for this event, to enhance the match data
-						res.log(thisFuncName + 'Pulling scoring data for teamKey=' + teamKey + ',event_key=' + event_key);
-						aggCol.find({"team_key": teamKey, "event_key": event_key}, {}, function (e, docs) {
-							// Build a map of match_key->data
-							var matchDataMap = {};
-							if (docs && docs.length > 0) {
-								for (var mDMidx = 0; mDMidx < docs.length; mDMidx++) {
-									var thisTeamMatch = docs[mDMidx];
-									//res.log(thisFuncName + 'Match scouting data for thisTeamMatch.match_key=' + thisTeamMatch.match_key);
-									if (thisTeamMatch.data)
+				// Pit data layout
+				scoutCol.find({ "year": event_year }, {sort: {"order": 1}}, function(e, docs){
+					var layout = docs;
+					//res.log(thisFuncName + 'layout=' + JSON.stringify(layout));
+					
+					// Pull in individual scouting data for this team, for this event, to enhance the match data
+					res.log(thisFuncName + 'Pulling scoring data for teamKey=' + teamKey + ',event_key=' + event_key);
+					aggCol.find({"team_key": teamKey, "event_key": event_key}, {}, function (e, docs) {
+						// Build a map of match_key->data
+						var matchDataMap = {};
+						if (docs && docs.length > 0) {
+							for (var mDMidx = 0; mDMidx < docs.length; mDMidx++) {
+								var thisTeamMatch = docs[mDMidx];
+								//res.log(thisFuncName + 'Match scouting data for thisTeamMatch.match_key=' + thisTeamMatch.match_key);
+								if (thisTeamMatch.data)
+								{
+									//res.log(thisFuncName + 'Adding data to map');
+									matchDataMap[thisTeamMatch.match_key] = thisTeamMatch.data;
+								}
+							}
+						}
+								
+						// Match history info
+						matchCol.find({"alliances.red.score": { $ne: -1}, "event_key" : event_key, $or: [{"alliances.blue.team_keys": teamKey}, {"alliances.red.team_keys": teamKey}]}, {sort: {time: -1}}, function (e, docs) {
+							var matches = docs;
+							if (matches && matches.length > 0) {
+								for (var matchesIdx = 0; matchesIdx < matches.length; matchesIdx++) {
+									//res.log(thisFuncName + 'For match ' + matches[matchesIdx].key);
+									var thisScoreData = matchDataMap[matches[matchesIdx].key];
+									if (thisScoreData)
 									{
-										//res.log(thisFuncName + 'Adding data to map');
-										matchDataMap[thisTeamMatch.match_key] = thisTeamMatch.data;
+										//res.log(thisFuncName + 'Enhancing match #' + matchesIdx + ': match_key=' + matches[matchesIdx].match_key + ', thisScoreData=' + JSON.stringify(thisScoreData));
+										matches[matchesIdx].scoringdata = thisScoreData;
 									}
 								}
 							}
-									
-							// Match history info
-							matchCol.find({"alliances.red.score": { $ne: -1}, "event_key" : event_key, $or: [{"alliances.blue.team_keys": teamKey}, {"alliances.red.team_keys": teamKey}]}, {sort: {time: -1}}, function (e, docs) {
-								var matches = docs;
-								if (matches && matches.length > 0) {
-									for (var matchesIdx = 0; matchesIdx < matches.length; matchesIdx++) {
-										//res.log(thisFuncName + 'For match ' + matches[matchesIdx].key);
-										var thisScoreData = matchDataMap[matches[matchesIdx].key];
-										if (thisScoreData)
-										{
-											//res.log(thisFuncName + 'Enhancing match #' + matchesIdx + ': match_key=' + matches[matchesIdx].match_key + ', thisScoreData=' + JSON.stringify(thisScoreData));
-											matches[matchesIdx].scoringdata = thisScoreData;
-										}
+							//res.log(thisFuncName + 'matches=' + JSON.stringify(matches));
+					
+							// Match data layout - use to build dynamic Mongo aggregation query
+							// db.scoringdata.aggregate( [ 
+							// { $match : { "data":{$exists:true}, "event_key": "2018njfla", "team_key": "frc303" } }, 
+							// { $group : { _id: "$team_key",
+							// "teleScaleMIN": {$min: "$data.teleScale"},
+							// "teleScaleAVG": {$avg: "$data.teleScale"},
+							// "teleScaleMAX": {$max: "$data.teleScale"}
+							//  } }
+							// ] );						
+							scoreCol.find({}, {sort: {"order": 1}}, function(e, docs){
+								var scorelayout = docs;
+								var aggQuery = [];
+								aggQuery.push({ $match : { "data":{$exists:true}, "event_key": event_key, "team_key": teamKey } });
+								var groupClause = {};
+								groupClause["_id"] = "$team_key";
+
+								for (var scoreIdx = 0; scoreIdx < scorelayout.length; scoreIdx++) {
+									var thisLayout = scorelayout[scoreIdx];
+									if (thisLayout.type == 'checkbox' || thisLayout.type == 'counter' || thisLayout.type == 'badcounter') {
+										//res.log(thisFuncName + 'thisLayout.type=' + thisLayout.type + ', thisLayout.id=' + thisLayout.id);
+										groupClause[thisLayout.id + "MIN"] = {$min: "$data." + thisLayout.id};
+										groupClause[thisLayout.id + "AVG"] = {$avg: "$data." + thisLayout.id};
+										groupClause[thisLayout.id + "VAR"] = {$stdDevPop: "$data." + thisLayout.id};
+										groupClause[thisLayout.id + "MAX"] = {$max: "$data." + thisLayout.id};
 									}
 								}
-								//res.log(thisFuncName + 'matches=' + JSON.stringify(matches));
-						
-								// Match data layout - use to build dynamic Mongo aggregation query
-								// db.scoringdata.aggregate( [ 
-								// { $match : { "data":{$exists:true}, "event_key": "2018njfla", "team_key": "frc303" } }, 
-								// { $group : { _id: "$team_key",
-								// "teleScaleMIN": {$min: "$data.teleScale"},
-								// "teleScaleAVG": {$avg: "$data.teleScale"},
-								// "teleScaleMAX": {$max: "$data.teleScale"}
-								//  } }
-								// ] );						
-								scoreCol.find({}, {sort: {"order": 1}}, function(e, docs){
-									var scorelayout = docs;
-									var aggQuery = [];
-									aggQuery.push({ $match : { "data":{$exists:true}, "event_key": event_key, "team_key": teamKey } });
-									var groupClause = {};
-									groupClause["_id"] = "$team_key";
+								aggQuery.push({ $group: groupClause });
+								//res.log(thisFuncName + 'aggQuery=' + JSON.stringify(aggQuery));
+								
+								aggCol.aggregate(aggQuery, function(e, docs){
+									var aggresult = {};
+									if (docs && docs[0])
+										aggresult = docs[0];
+									//res.log(thisFuncName + 'aggresult=' + JSON.stringify(aggresult));
 
+									// Unspool single row of aggregate results into tabular form
+									var aggTable = [];
 									for (var scoreIdx = 0; scoreIdx < scorelayout.length; scoreIdx++) {
 										var thisLayout = scorelayout[scoreIdx];
 										if (thisLayout.type == 'checkbox' || thisLayout.type == 'counter' || thisLayout.type == 'badcounter') {
-											//res.log(thisFuncName + 'thisLayout.type=' + thisLayout.type + ', thisLayout.id=' + thisLayout.id);
-											groupClause[thisLayout.id + "MIN"] = {$min: "$data." + thisLayout.id};
-											groupClause[thisLayout.id + "AVG"] = {$avg: "$data." + thisLayout.id};
-											groupClause[thisLayout.id + "VAR"] = {$stdDevPop: "$data." + thisLayout.id};
-											groupClause[thisLayout.id + "MAX"] = {$max: "$data." + thisLayout.id};
+											var aggRow = {};
+											aggRow['key'] = thisLayout.id;
+											
+											// Recompute VAR first = StdDev/Mean
+											aggRow['var'] = aggRow['var'] / (aggRow['avg'] + 0.001);
+							
+											aggRow['min'] = (Math.round(aggresult[thisLayout.id + "MIN"] * 10)/10).toFixed(1);
+											aggRow['avg'] = (Math.round(aggresult[thisLayout.id + "AVG"] * 10)/10).toFixed(1);
+											aggRow['var'] = (Math.round(aggresult[thisLayout.id + "VAR"] * 10)/10).toFixed(1);
+											aggRow['max'] = (Math.round(aggresult[thisLayout.id + "MAX"] * 10)/10).toFixed(1);
+											aggTable.push(aggRow);
 										}
 									}
-									aggQuery.push({ $group: groupClause });
-									//res.log(thisFuncName + 'aggQuery=' + JSON.stringify(aggQuery));
-									
-									aggCol.aggregate(aggQuery, function(e, docs){
-										var aggresult = {};
-										if (docs && docs[0])
-											aggresult = docs[0];
-										//res.log(thisFuncName + 'aggresult=' + JSON.stringify(aggresult));
+									//res.log(thisFuncName + 'aggTable=' + JSON.stringify(aggTable));
 
-										// Unspool single row of aggregate results into tabular form
-										var aggTable = [];
-										for (var scoreIdx = 0; scoreIdx < scorelayout.length; scoreIdx++) {
-											var thisLayout = scorelayout[scoreIdx];
-											if (thisLayout.type == 'checkbox' || thisLayout.type == 'counter' || thisLayout.type == 'badcounter') {
-												var aggRow = {};
-												aggRow['key'] = thisLayout.id;
-												
-												// Recompute VAR first = StdDev/Mean
-												aggRow['var'] = aggRow['var'] / (aggRow['avg'] + 0.001);
-								
-												aggRow['min'] = (Math.round(aggresult[thisLayout.id + "MIN"] * 10)/10).toFixed(1);
-												aggRow['avg'] = (Math.round(aggresult[thisLayout.id + "AVG"] * 10)/10).toFixed(1);
-												aggRow['var'] = (Math.round(aggresult[thisLayout.id + "VAR"] * 10)/10).toFixed(1);
-												aggRow['max'] = (Math.round(aggresult[thisLayout.id + "MAX"] * 10)/10).toFixed(1);
-												aggTable.push(aggRow);
-											}
-										}
-										//res.log(thisFuncName + 'aggTable=' + JSON.stringify(aggTable));
+									//res.log(thisFuncName + 'pitData=' + JSON.stringify(pitData));
+									//res.log(thisFuncName + 'pitData1=' + JSON.stringify(pitData1));
 
-										//res.log(thisFuncName + 'pitData=' + JSON.stringify(pitData));
-										//res.log(thisFuncName + 'pitData1=' + JSON.stringify(pitData1));
-
-										res.render("./reports/teamintel", {
-											title: "Intel: Team " + teamKey.substring(3),
-											team: team,
-											ranking: ranking,
-											data: pitData,
-											data1: pitData1,
-											layout: layout,
-											scorelayout: scorelayout,
-											aggdata: aggTable,
-											matches: matches
-										});
+									res.render("./reports/teamintel", {
+										title: "Intel: Team " + teamKey.substring(3),
+										team: team,
+										ranking: ranking,
+										data: pitData,
+										data1: pitData1,
+										layout: layout,
+										scorelayout: scorelayout,
+										aggdata: aggTable,
+										matches: matches
 									});
 								});
 							});

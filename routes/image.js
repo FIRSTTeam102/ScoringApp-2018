be9d98d255589e1466b7e49725283b7e57aa0958
Jordan Lees <firstteam102@gmail.com>
2019-02-28 18:24:32 -0500

    fixed indenting (no code has been modified)

--- routes/image.js
+++ routes/image.js
@@ -17,35 +17,35 @@ var AvatarStorage = require('../helpers/AvatarStorage');
 
 // setup a new instance of the AvatarStorage engine 
 var storage = AvatarStorage({
-square: true,
-responsive: true,
-greyscale: true,
-quality: 90
+    square: true,
+    responsive: true,
+    greyscale: true,
+    quality: 90
 });
 
 var limits = {
-files: 1, // allow only 1 file per request
-fileSize: 1024 * 1024, // 1 MB (max file size)
+    files: 1, // allow only 1 file per request
+    fileSize: 1024 * 1024, // 1 MB (max file size)
 };
 
 var fileFilter = function(req, file, cb) {
-// supported image file mimetypes
-var allowedMimes = ['image/jpeg', 'image/pjpeg', 'image/png', 'image/gif'];
+    // supported image file mimetypes
+    var allowedMimes = ['image/jpeg', 'image/pjpeg', 'image/png', 'image/gif'];
 
-if (_.includes(allowedMimes, file.mimetype)) {
-// allow supported image files
-cb(null, true);
-} else {
-// throw error for invalid files
-cb(new Error('Invalid file type. Only jpg, png and gif image files are allowed.'));
-}
+    if (_.includes(allowedMimes, file.mimetype)) {
+        // allow supported image files
+        cb(null, true);
+    } else {
+        // throw error for invalid files
+        cb(new Error('Invalid file type. Only jpg, png and gif image files are allowed.'));
+    }
 };
 
 // setup multer
 var upload = multer({
-storage: storage,
-limits: limits,
-fileFilter: fileFilter
+    storage: storage,
+    limits: limits,
+    fileFilter: fileFilter
 });
 
 
@@ -62,32 +62,34 @@ router.get('/', function(req, res, next) {
 /* More pasted code... ... */////////////////////////////////////////
 
 router.post('/upload', upload.single("avatarfield"), function(req, res, next) {
+    
     console.log("process.env.AVATAR_FIELD =" + process.env.AVATAR_FIELD);
     var files;
     var file = req.file.filename;
     var matches = file.match(/^(.+?)_.+?\.(.+)$/i);
     
     if (matches) {
-    files = _.map(['lg', 'md', 'sm'], function(size) {
-    return matches[1] + '_' + size + '.' + matches[2];
-    });
-    } else {
-    files = [file];
+        files = _.map(['lg', 'md', 'sm'], function(size) {
+            return matches[1] + '_' + size + '.' + matches[2];
+        });
+    } 
+    else{
+        files = [file];
     }
     
     files = _.map(files, function(file) {
-    var port = req.app.get('port');
-    var base = req.protocol + '://' + req.hostname + (port ? ':' + port : '');
-    var url = path.join(req.file.baseUrl, file).replace(/[\\\/]+/g, '/').replace(/^[\/]+/g, '');
-    
-    return (req.file.storage == 'local' ? base : '') + '/' + url;
+        var port = req.app.get('port');
+        var base = req.protocol + '://' + req.hostname + (port ? ':' + port : '');
+        var url = path.join(req.file.baseUrl, file).replace(/[\\\/]+/g, '/').replace(/^[\/]+/g, '');
+        
+        return (req.file.storage == 'local' ? base : '') + '/' + url;
     });
     
     res.json({
-    images: files
+        images: files
     });
     
-    });
+});
 
 
 module.exports = router;

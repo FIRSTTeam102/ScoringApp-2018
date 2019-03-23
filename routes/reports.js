6ffb361ab092b928d4fe475884a02af82ff60a5b
Mike O'Connell <michael.p.oconnell@gmail.com>
2019-03-23 07:11:20 -0400

    Added "All Team Metrics" (copy of Alliance Selection Data but with AVG/MAX toggle)

--- routes/reports.js
+++ routes/reports.js
@@ -1124,4 +1124,111 @@ router.get("/metricintel*", function(req, res){
 	});	
 });
 
+/**
+ * Metrics view
+ */
+router.get("/allteammetrics", function(req, res){
+	var thisFuncName = "reports.allteammetrics[get]: ";
+	res.log(thisFuncName + 'ENTER');
+	
+	var db = req.db;
+	var aggCol = req.db.get('scoringdata');
+	var scoreCol = db.get("scoringlayout");
+	var rankCol = db.get('currentrankings');
+	// 2019-03-21, M.O'C: Utilize the currentaggranges
+	var currentAggCol = db.get("currentaggranges");
+	
+	// for later querying by event_key
+	var event_key = req.event.key;
+	var event_year = req.event.year;
+	res.log(thisFuncName + 'event_key=' + event_key);
+	
+	// get the current rankings
+	rankCol.find({}, {}, function (e, docs) {
+		var rankings = [];
+		if (docs)
+			rankings = docs;
+		var rankMap = {};
+		for (var rankIdx = 0; rankIdx < rankings.length; rankIdx++) {
+			//res.log(thisFuncName + 'rankIdx=' + rankIdx + ', team_key=' + rankings[rankIdx].team_key + ', rank=' + rankings[rankIdx].rank);
+			rankMap[rankings[rankIdx].team_key] = rankings[rankIdx];
+		}
+		//res.log(thisFuncName + 'rankMap=' + JSON.stringify(rankMap));
+
+		// Match data layout - use to build dynamic Mongo aggregation query  --- Comboing twice, on two sets of team keys: red alliance & blue alliance
+		// db.scoringdata.aggregate( [ 
+		// { $match : { "team_key":{$in: [...]}, "event_key": event_key } }, 
+		// { $group : { _id: "$event_key",
+		// "autoScaleAVG": {$avg: "$data.autoScale"},
+		// "teleScaleAVG": {$avg: "$data.teleScale"},
+		//  } }
+		// ] );						
+		scoreCol.find({ "year": event_year }, {sort: {"order": 1}}, function(e, docs){
+			var scorelayout = docs;
+			
+			var aggQuery = [];
+			aggQuery.push({ $match : { "event_key": event_key } });
+			var groupClause = {};
+			// group teams for 1 row per team
+			groupClause["_id"] = "$team_key";
+
+			for (var scoreIdx = 0; scoreIdx < scorelayout.length; scoreIdx++) {
+				var thisLayout = scorelayout[scoreIdx];
+				thisLayout.key = thisLayout.id;
+				scorelayout[scoreIdx] = thisLayout;
+				if (thisLayout.type == 'checkbox' || thisLayout.type == 'counter' || thisLayout.type == 'badcounter')
+				{
+					groupClause[thisLayout.id + "AVG"] = {$avg: "$data." + thisLayout.id};
+					groupClause[thisLayout.id + "MAX"] = {$max: "$data." + thisLayout.id};
+				}
+			}
+			aggQuery.push({ $group: groupClause });
+			aggQuery.push({ $sort: { _id: 1 } });
+			//res.log(thisFuncName + 'aggQuery=' + JSON.stringify(aggQuery));
+			
+			aggCol.aggregate(aggQuery, function(e, docs){
+				var aggArray = [];
+				if (docs)
+					aggArray = docs;
+					
+				res.log(rankMap);
+				
+				// Rewrite data into display-friendly values
+				for (var aggIdx = 0; aggIdx < aggArray.length; aggIdx++) {
+					var thisAgg = aggArray[aggIdx];
+					for (var scoreIdx = 0; scoreIdx < scorelayout.length; scoreIdx++) {
+						var thisLayout = scorelayout[scoreIdx];
+						if (thisLayout.type == 'checkbox' || thisLayout.type == 'counter' || thisLayout.type == 'badcounter') {
+							var roundedValAvg = (Math.round(thisAgg[thisLayout.id + "AVG"] * 10)/10).toFixed(1);
+							var roundedValMax = (Math.round(thisAgg[thisLayout.id + "MAX"] * 10)/10).toFixed(1);
+							thisAgg[thisLayout.id + "AVG"] = roundedValAvg;
+							thisAgg[thisLayout.id + "MAX"] = roundedValMax;
+						}
+					}
+					if(rankMap[thisAgg._id]){
+						thisAgg['rank'] = rankMap[thisAgg._id].rank;
+						thisAgg['value'] = rankMap[thisAgg._id].value;
+						aggArray[aggIdx] = thisAgg;
+					}
+				}
+				res.log(thisFuncName + 'aggArray=' + JSON.stringify(aggArray));
+
+				// read in the current agg ranges
+				currentAggCol.find({}, {}, function (e, docs) {
+					var currentAggRanges = [];
+					if (docs)
+						currentAggRanges = docs;
+
+					res.render("./reports/allteammetrics", {
+						title: "Alliance Selection",
+						aggdata: aggArray,
+						currentAggRanges: currentAggRanges,
+						layout: scorelayout
+					});
+				});
+			});
+		});
+	});
+});
+
 module.exports = router;

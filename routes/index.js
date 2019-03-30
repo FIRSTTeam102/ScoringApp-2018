8ed40b33a4fd4f2ae8f930cf989a6e4d0434577d
Jordan Lees <firstteam102@gmail.com>
2019-03-30 16:27:52 -0400

    test 3

--- routes/index.js
+++ routes/index.js
@@ -26,17 +26,16 @@ router.post('/webhook', function(req, res){
 		console.log("body: "+typeof(req.body));
 		console.log("message_type: "+typeof(req.body.message_type));
 		console.log("message_data: "+typeof(req.body.message_data));
+		
 		switch(req.body.message_type){
 			case "match_score":
 				updateMatch(req, res);
+				res.sendStatus(200);
 				break;
 			case "ping":
 				res.sendStatus(200).send("pong!");
 		}
 	}
-	
-	
-	res.send(200);
 });
 
 function updateRankings(req, res){
@@ -79,7 +78,7 @@ function updateRankings(req, res){
 
 function updateMatch(req, res){
 	
-	res.log("inside updateMatch function");
+	//2019-03-30 JL: Copying from current/updatematch for webhook handling
 	
 	var db = req.db;
 	var matchCol = db.get("matches");
@@ -94,112 +93,110 @@ function updateMatch(req, res){
 	var event_key = req.event.key;
 	
 	var data = req.body.message_data;
-	res.log("datastring:");
+	res.log("data:");
 	res.log(data);
 	
-	var match = JSON.parse(data);
-	res.log("data json: ");
-	res.log(match);
-	// stick it in an array so the insert will work later
-	var array = [];
-	array.push(match);
-	
-	// Now, insert the new object
-	matchCol.insert(array, function(e, docs) {
-		// Then read all the matches back in order
-		matchCol.find({"event_key": eventId},{sort: {"time": 1}}, function(e, docs){
-			var matches = docs;
-			
-			//
-			// 2019-03-21, M.O'C: Adding in recalculation of aggregation data
-			//
-			res.log(thisFuncName + 'About to start in on updating min/maxes of agg data');
-			scoreCol.find({ "year": event_year }, {sort: {"order": 1}}, function(e, docs){
-				var scorelayout = docs;
+	//if data is an object, proceed
+	if(typeof(data) == "object"){
+		
+		res.log(`Match update pushed for ${data.match_key}`, true);
+		
+		// stick it in an array so the insert will work later
+		var match = data;
+		var array = [match];
+		
+		// Now, insert the new object
+		matchCol.insert(array, function(e, docs) {
+			// Then read all the matches back in order
+			matchCol.find({"event_key": eventId},{sort: {"time": 1}}, function(e, docs){
+				var matches = docs;
 				
-				var aggQuery = [];
-				aggQuery.push({ $match : { "event_key": event_key } });
-				var groupClause = {};
-				// group teams for 1 row per team
-				groupClause["_id"] = "$team_key";
-	
-				for (var scoreIdx = 0; scoreIdx < scorelayout.length; scoreIdx++) {
-					var thisLayout = scorelayout[scoreIdx];
-					thisLayout.key = thisLayout.id;
-					scorelayout[scoreIdx] = thisLayout;
-					if (thisLayout.type == 'checkbox' || thisLayout.type == 'counter' || thisLayout.type == 'badcounter')
-					{
-						groupClause[thisLayout.id + "MIN"] = {$min: "$data." + thisLayout.id};
-						groupClause[thisLayout.id + "AVG"] = {$avg: "$data." + thisLayout.id};
-						groupClause[thisLayout.id + "VAR"] = {$stdDevPop: "$data." + thisLayout.id};
-						groupClause[thisLayout.id + "MAX"] = {$max: "$data." + thisLayout.id};
-					}
-				}
-				aggQuery.push({ $group: groupClause });
-				aggQuery.push({ $sort: { _id: 1 } });
-				res.log(thisFuncName + 'aggQuery=' + JSON.stringify(aggQuery));
-
-				// Run the aggregation!
-				aggCol.aggregate(aggQuery, function(e, docs){
-					var aggArray = [];
-					if (docs)
-						aggArray = docs;
-						
-					//res.log(rankMap);
-
-					var aggMinMaxArray = [];
-
-					// Cycle through & build a map of min/max values per scoring type per aggregation
+				// 2019-03-21, M.O'C: Adding in recalculation of aggregation data
+				res.log(thisFuncName + 'About to start in on updating min/maxes of agg data');
+				scoreCol.find({ "year": event_year }, {sort: {"order": 1}}, function(e, docs){
+					var scorelayout = docs;
+					
+					var aggQuery = [];
+					aggQuery.push({ $match : { "event_key": event_key } });
+					var groupClause = {};
+					// group teams for 1 row per team
+					groupClause["_id"] = "$team_key";
+		
 					for (var scoreIdx = 0; scoreIdx < scorelayout.length; scoreIdx++) {
 						var thisLayout = scorelayout[scoreIdx];
+						thisLayout.key = thisLayout.id;
+						scorelayout[scoreIdx] = thisLayout;
 						if (thisLayout.type == 'checkbox' || thisLayout.type == 'counter' || thisLayout.type == 'badcounter')
 						{
-							var thisMinMax = {};
-
-							// initialize ranges
-							var MINmin = 999999; var MINmax = 0; var AVGmin = 999999; var AVGmax = 0; var VARmin = 999999; var VARmax = 0; var MAXmin = 999999; var MAXmax = 0;
-							// cycle through all the per-team aggregated data
-							for (var aggIdx = 0; aggIdx < aggArray.length; aggIdx++) {
-								var thisAgg = aggArray[aggIdx];
-								var roundedMinVal = (Math.round(thisAgg[thisLayout.id + "MIN"] * 10)/10).toFixed(1);
-								var roundedAvgVal = (Math.round(thisAgg[thisLayout.id + "AVG"] * 10)/10).toFixed(1);
-								var roundedVarVal = (Math.round(thisAgg[thisLayout.id + "VAR"] * 10)/10).toFixed(1);
-								var roundedMaxVal = (Math.round(thisAgg[thisLayout.id + "MAX"] * 10)/10).toFixed(1);
-
-								if (roundedMinVal < MINmin) MINmin = roundedMinVal; if (roundedMinVal > MINmax) MINmax = roundedMinVal; 
-								if (roundedAvgVal < AVGmin) AVGmin = roundedAvgVal; if (roundedAvgVal > AVGmax) AVGmax = roundedAvgVal; 
-								if (roundedVarVal < VARmin) VARmin = roundedVarVal; if (roundedVarVal > VARmax) VARmax = roundedVarVal; 
-								if (roundedMaxVal < MAXmin) MAXmin = roundedMaxVal; if (roundedMaxVal > MAXmax) MAXmax = roundedMaxVal; 
-							}
-
-							thisMinMax['key'] = thisLayout.key;
-							thisMinMax['MINmin'] = MINmin; thisMinMax['MINmax'] = MINmax;
-							thisMinMax['AVGmin'] = AVGmin; thisMinMax['AVGmax'] = AVGmax;
-							thisMinMax['VARmin'] = VARmin; thisMinMax['VARmax'] = VARmax;
-							thisMinMax['MAXmin'] = MAXmin; thisMinMax['MAXmax'] = MAXmax;
-
-							res.log(thisFuncName + 'thisMinMax=' + JSON.stringify(thisMinMax));
-
-							aggMinMaxArray.push(thisMinMax);
+							groupClause[thisLayout.id + "MIN"] = {$min: "$data." + thisLayout.id};
+							groupClause[thisLayout.id + "AVG"] = {$avg: "$data." + thisLayout.id};
+							groupClause[thisLayout.id + "VAR"] = {$stdDevPop: "$data." + thisLayout.id};
+							groupClause[thisLayout.id + "MAX"] = {$max: "$data." + thisLayout.id};
 						}
 					}
-					console.log(thisFuncName + 'aggMinMaxArray=' + JSON.stringify(aggMinMaxArray));
+					aggQuery.push({ $group: groupClause });
+					aggQuery.push({ $sort: { _id: 1 } });
+					res.log(thisFuncName + 'aggQuery=' + JSON.stringify(aggQuery));
 
-					// Delete the current agg ranges
-					currentAggCol.remove({}, function(e, docs) {
-						// Reinsert the updated values
-						currentAggCol.insert(aggMinMaxArray, function(e, docs) {
-							// And we're done!
-							res.render("./admin/currentmatches", {
-								title: "Matches",
-								"matches": matches
+					// Run the aggregation!
+					aggCol.aggregate(aggQuery, function(e, docs){
+						var aggArray = [];
+						if (docs)
+							aggArray = docs;
+							
+						res.log(rankMap);
+
+						var aggMinMaxArray = [];
+
+						// Cycle through & build a map of min/max values per scoring type per aggregation
+						for (var scoreIdx = 0; scoreIdx < scorelayout.length; scoreIdx++) {
+							var thisLayout = scorelayout[scoreIdx];
+							if (thisLayout.type == 'checkbox' || thisLayout.type == 'counter' || thisLayout.type == 'badcounter')
+							{
+								var thisMinMax = {};
+
+								// initialize ranges
+								var MINmin = 999999; var MINmax = 0; var AVGmin = 999999; var AVGmax = 0; var VARmin = 999999; var VARmax = 0; var MAXmin = 999999; var MAXmax = 0;
+								// cycle through all the per-team aggregated data
+								for (var aggIdx = 0; aggIdx < aggArray.length; aggIdx++) {
+									var thisAgg = aggArray[aggIdx];
+									var roundedMinVal = (Math.round(thisAgg[thisLayout.id + "MIN"] * 10)/10).toFixed(1);
+									var roundedAvgVal = (Math.round(thisAgg[thisLayout.id + "AVG"] * 10)/10).toFixed(1);
+									var roundedVarVal = (Math.round(thisAgg[thisLayout.id + "VAR"] * 10)/10).toFixed(1);
+									var roundedMaxVal = (Math.round(thisAgg[thisLayout.id + "MAX"] * 10)/10).toFixed(1);
+
+									if (roundedMinVal < MINmin) MINmin = roundedMinVal; if (roundedMinVal > MINmax) MINmax = roundedMinVal; 
+									if (roundedAvgVal < AVGmin) AVGmin = roundedAvgVal; if (roundedAvgVal > AVGmax) AVGmax = roundedAvgVal; 
+									if (roundedVarVal < VARmin) VARmin = roundedVarVal; if (roundedVarVal > VARmax) VARmax = roundedVarVal; 
+									if (roundedMaxVal < MAXmin) MAXmin = roundedMaxVal; if (roundedMaxVal > MAXmax) MAXmax = roundedMaxVal; 
+								}
+
+								thisMinMax['key'] = thisLayout.key;
+								thisMinMax['MINmin'] = MINmin; thisMinMax['MINmax'] = MINmax;
+								thisMinMax['AVGmin'] = AVGmin; thisMinMax['AVGmax'] = AVGmax;
+								thisMinMax['VARmin'] = VARmin; thisMinMax['VARmax'] = VARmax;
+								thisMinMax['MAXmin'] = MAXmin; thisMinMax['MAXmax'] = MAXmax;
+
+								res.log(thisFuncName + 'thisMinMax=' + JSON.stringify(thisMinMax));
+
+								aggMinMaxArray.push(thisMinMax);
+							}
+						}
+						console.log(thisFuncName + 'aggMinMaxArray=' + JSON.stringify(aggMinMaxArray));
+
+						// Delete the current agg ranges
+						currentAggCol.remove({}, function(e, docs) {
+							// Reinsert the updated values
+							currentAggCol.insert(aggMinMaxArray, function(e, docs) {
+								// And we're done!
+								//2019-03-30 JL: No page-rendering necessary, for a bot sent us this req
 							});
 						});
 					});
 				});
 			});
 		});
-	});
+	}
 }
 
 /**
